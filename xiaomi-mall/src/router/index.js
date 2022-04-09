b8ad41516401b8c1d0829ced95115501be864e42
Asa_zr <926197502@qq.com>
2022-04-09 14:50:38 +0800

    修复request.js

--- xiaomi-mall/src/router/index.js
+++ xiaomi-mall/src/router/index.js
@@ -1,31 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import 'nprogress/nprogress.css'
-import NProgress from 'nprogress'
-Vue.use(VueRouter)
-// Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  NProgress.start()
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
 
-// Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  NProgress.done()
-  return response;
-}, function (error) {
-  NProgress.done()
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  return Promise.reject(error);
-});
+Vue.use(VueRouter)
+
 const routes = [{
     path: '/',
     name: 'home',
--- xiaomi-mall/src/utils/request.js
+++ xiaomi-mall/src/utils/request.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
 export const serveUrl = 'https://grumpy-fireant-26.loca.lt'
 export const instance = axios.create({
   baseURL: serveUrl,
@@ -13,3 +15,26 @@ export const post = (url, params) => instance.post(url, params)
 export const put = (url, params) => instance.put(url, params)
 
 export const dele = (url, params) => instance.delete(url, params)
+
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  NProgress.start()
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  NProgress.done()
+  return response;
+}, function (error) {
+  NProgress.done()
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});

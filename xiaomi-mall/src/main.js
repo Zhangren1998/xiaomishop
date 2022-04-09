6b655aa2afc1cdfd4c3b01fa6ed0effbcbf33879
Asa_zr <926197502@qq.com>
2022-04-09 13:55:00 +0800

    chore:引入vant

--- xiaomi-mall/src/main.js
+++ xiaomi-mall/src/main.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
 import App from './App.vue'
 import router from './router'
 import store from './store'

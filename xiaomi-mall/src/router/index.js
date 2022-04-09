83223206444e7b9bf0952568fe9d2df320c53e12
Asa_zr <926197502@qq.com>
2022-04-09 14:47:27 +0800

    chore:封装axios功能

--- xiaomi-mall/src/router/index.js
+++ xiaomi-mall/src/router/index.js
@@ -1,11 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
 Vue.use(VueRouter)
+// Add a request interceptor
+axios.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  NProgress.start()
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
 
-const routes = [
-  {
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  NProgress.done()
+  return response;
+}, function (error) {
+  NProgress.done()
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
+const routes = [{
     path: '/',
     name: 'home',
     component: HomeView
@@ -16,7 +37,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 

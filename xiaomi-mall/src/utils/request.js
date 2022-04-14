dbe44221471af11647469188ab2e933ebf55a389
brr <2498339388@qq.com>
2022-04-14 14:56:51 +0800

    brr

--- xiaomi-mall/src/utils/request.js
+++ xiaomi-mall/src/utils/request.js
@@ -1,48 +1,55 @@
-import axios from 'axios';
-import 'nprogress/nprogress.css'
-import NProgress from 'nprogress'
+import axios from "axios";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
 // export const serveUrl = 'https://brown-duck-8.loca.lt'
-// export const serveUrl = ' https://weak-robin-43.loca.lt'
-export const serveUrl = 'http://192.168.1.9:1337'
+export const serveUrl = " https://breezy-lion-92.loca.lt";
+// export const serveUrl = 'http://192.168.1.9:1337'
 
 export const instance = axios.create({
   baseURL: serveUrl,
   // timeout: 5000,
-})
-export const get = (url, params) => instance.get(url, {
-  params
-})
+});
+export const get = (url, params) =>
+  instance.get(url, {
+    params,
+  });
 
-export const post = (url, params) => instance.post(url, params)
+export const post = (url, params) => instance.post(url, params);
 
-export const put = (url, params) => instance.put(url, params)
+export const put = (url, params) => instance.put(url, params);
 
-export const dele = (url, params) => instance.delete(url, params)
+export const dele = (url, params) => instance.delete(url, params);
 
 // Add a request interceptor
-instance.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  NProgress.start()
-  let token = sessionStorage.getItem("token"); //获取到从后端传来的token，然后存到session里面
-  if (token) {
-    config.headers.token = token; //然后赋值就行
-  }
+instance.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    NProgress.start();
+    let token = sessionStorage.getItem("token"); //获取到从后端传来的token，然后存到session里面
+    if (token) {
+      config.headers.token = token; //然后赋值就行
+    }
 
-  return config;
-}, function (error) {
-  // Do something with request error
-  return Promise.reject(error);
-});
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
 
 // Add a response interceptor
-instance.interceptors.response.use(function (response) {
-  // Any status code that lie within the range of 2xx cause this function to trigger
-  // Do something with response data
-  NProgress.done()
-  return response;
-}, function (error) {
-  NProgress.done()
-  // Any status codes that falls outside the range of 2xx cause this function to trigger
-  // Do something with response error
-  return Promise.reject(error);
-});
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    NProgress.done();
+    return response;
+  },
+  function (error) {
+    NProgress.done();
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
